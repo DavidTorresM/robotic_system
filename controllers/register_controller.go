46609a282155966caddbe514304c7bc4a6421111
David Torres M <dtorresm1200@alumno.ipn.mx>
2025-02-05 00:05:25 -0600

    HOTFIX: Ocultando Password

--- controllers/register_controller.go
+++ controllers/register_controller.go
@@ -2,28 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"robotica_concursos/models"
 	"robotica_concursos/services"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Participante struct {
-	ID       uint    `gorm:"primaryKey"`
-	Nombre   string  `gorm:"size:100;not null"`
-	Correo   string  `gorm:"size:100;unique;not null"`
-	Password string  `gorm:"size:100;" json:"Password,omitempty"`
-	Telefono string  `gorm:"size:15"`
-	EquipoID uint    `gorm:"not null"`
-	Equipo   *Equipo `gorm:"foreignKey:EquipoID" json:"Equipo,omitempty"`
-}
-
-type Equipo struct {
-	ID   uint   `gorm:"primaryKey"`
-	Name string `gorm:"size:100;not null"`
-}
-
 func RegisterParticipante(c *gin.Context) {
-	var participante Participante
+	var participante models.Participante
 	if err := c.ShouldBindJSON(&participante); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -34,7 +20,7 @@ func RegisterParticipante(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
+	participante.Password = ""
 	c.JSON(http.StatusOK, gin.H{"data": participante})
 }
 
