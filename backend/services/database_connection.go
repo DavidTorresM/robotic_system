7706db36035b9fa2123c5a111e4dd848442bbaca
David Torres M <dtorresm1200@alumno.ipn.mx>
2025-02-21 22:03:07 -0600

    FIX: Reintentos para conectar a la DB

--- backend/services/database_connection.go
+++ backend/services/database_connection.go
@@ -3,11 +3,13 @@ package services
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var timeout_conexion = 60
 var DB *gorm.DB
 
 func ConnectDatabase() {
@@ -16,13 +18,22 @@ func ConnectDatabase() {
 	if dsn == "" {
 		log.Fatal("DATABASE_DSN environment variable is not set")
 	}
-
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+	var conectada bool
+	for !conectada {
+		database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Println("Failed to connect to database:", err)
+			timeout_conexion--
+			if timeout_conexion <= 0 {
+				log.Fatal("Timeout reached. Could not connect to database.")
+			}
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		conectada = true
+		DB = database
 	}
 
-	DB = database
 }
 func GetDatabase() *gorm.DB {
 	if DB == nil {
